a8a8f2a66ac02b74df6b13ac6e5e87c5782bfd78
Omar <omar.padilla@integrify.com>
2019-05-21 15:04:49 -0500

    McMurder base

---
+++ planets/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id='planets'>
+    Hey
+    <transition name="fade" mode="out-in">
+      <router-view class="view"></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
